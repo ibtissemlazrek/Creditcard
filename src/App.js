252fab2a6eb11816768c3b466ba7bed5850f8613
ibtissemlazrek <ibtissem.lazreg@gmail.com>
2018-11-28 15:15:45 +0100

    jhjhdshds

--- src/App.js
+++ src/App.js
@@ -6,17 +6,33 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      number: ,
-      name:,
-      date:
+      number:"",
+      name:"",
+      date:""
 
     }
   }
-
-  onChange=()=>{
-    
+  changeNumber=(event)=>{
+    if((event.target.value.length<17) && (!isNaN(event.target.value)) ){
+    this.setState({   
+    number:event.target.value
+ })
+  } 
   }
-
+  changeName=(event)=>{
+    if(event.target.value.length<21 && ! /[0-9]/g.test(event.target.value)){
+    this.setState({
+      name:event.target.value
+    }) 
+  }
+  }
+    changeDate=(event)=>{
+      if((event.target.value.length<5)&&(!isNaN(event.target.value))){
+      this.setState({
+        date:event.target.value
+      })
+    }
+    }
   render() {
     return (<div className="main">
       <div className="app">
@@ -29,7 +45,7 @@ class App extends Component {
         </div>
         <div className="app-container">
           <div className="app-number">
-            <p className="app-serie"> 7253 3256 7895 1245</p>
+            <p className="app-serie">{this.state.number.padEnd(16,'*').split('').map((x,i)=>{return i%4==0?' '+x:x})  }</p>
 
             <div className="rese">
               <div className="app-num">
@@ -45,23 +61,23 @@ class App extends Component {
                     </div>
                     <div className="years"><p className="para">
                       <span className="months">MONTH/YEAR</span></p>
-                      <p className="num"> 11/50</p> </div>
+                      <p className="num"> {this.state.date.substring(0,2).padEnd(2,'*')+"/"+this.state.date.slice(2,4).padEnd(2,'*')}</p> </div>
                   </div>
                 </div>
               </div>
               <div className="logo"> <img width='100px' height='100px' className="log" src={image} /></div>
 
             </div>
-            <p className="txt"> CARDHOLDER</p>
+            <p className="txt"> {this.state.name.padEnd(20,'*').toUpperCase()}</p>
 
           </div>
         </div>
 
       </div>
       <div className="users">
-        <input type="text" className="input" placeholder=" CARD NUMBER" onChange="" />
-        <input type="text" className="input" placeholder=" YOUR NAME" />
-        <input type="text" className="input" placeholder=" XX/YY" />
+        <input type="text" className="input" placeholder=" CARD NUMBER" value={this.state.number} onChange={this.changeNumber}/>
+        <input type="text" className="input" placeholder=" YOUR NAME"  value={this.state.name}  onChange={this.changeName}/>
+        <input type="text" className="input" placeholder=" XX/YY"  value={this.state.date} onChange={this.changeDate}/>
 
       </div>
     </div>

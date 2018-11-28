9752b93c3e209537cae91b4cf497f9228b52f22a
ibtissemlazrek <ibtissem.lazreg@gmail.com>
2018-11-28 15:07:30 +0100

    Updates

---
+++ precache-manifest.077ff170acafd73fb4fcc53799a00885.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "10110331106a28a744628694b0993ba6",
+    "url": "/Creditcard/static/media/Capture1.10110331.PNG"
+  },
+  {
+    "revision": "0bdad86665c8d2f3cd36f95602f463da",
+    "url": "/Creditcard/static/media/Capture.0bdad866.PNG"
+  },
+  {
+    "revision": "18af2a9d36726a4d2837",
+    "url": "/Creditcard/static/js/runtime~main.18af2a9d.js"
+  },
+  {
+    "revision": "55c805870e4711fb774a",
+    "url": "/Creditcard/static/js/main.55c80587.chunk.js"
+  },
+  {
+    "revision": "a9d6ba5203de63d53915",
+    "url": "/Creditcard/static/js/1.a9d6ba52.chunk.js"
+  },
+  {
+    "revision": "55c805870e4711fb774a",
+    "url": "/Creditcard/static/css/main.c933d903.chunk.css"
+  },
+  {
+    "revision": "0faf69dc95a57b5d167351a57b9747ca",
+    "url": "/Creditcard/index.html"
+  }
+];
